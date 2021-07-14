066f3ad3b1edecd35f48182e466c3cff51e8675d
jothuma <86958080+jothuma@users.noreply.github.com>
2021-07-15 07:18:15 +0800

    Update app.js

--- app.js
+++ app.js
@@ -40,12 +40,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  secret: 'ñasddfilhpaf78h78032h780g780fg780asg780dsbovncubuyvqy',
-  cookie: {
-    secure: false,
-    maxAge: 99999999999
-  }
+//app.use(session({
+//  secret: 'ñasddfilhpaf78h78032h780g780fg780asg780dsbovncubuyvqy',
+//  cookie: {
+//    secure: false,
+//    maxAge: 99999999999
+//  }
 }));
 
 /*
